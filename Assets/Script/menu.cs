2ec9520ec4f9566803cde29d10fe4723d4df09be
pabloernestomartinez-collab <pabloernestomartinez@upc.edu.ar>
2025-09-30 21:44:00 -0300

    30092025pablo
    correccion del menu de inicio

--- Assets/Script/menu.cs
+++ Assets/Script/menu.cs
@@ -1,31 +1,41 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
     private Rigidbody2D playerRB;
-    private float speed = 3f;
+    private float speed = 20f;
     private Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerRB = GetComponent<Rigidbody2D>();
-        Debug.Log("sInicia el juego");
-
+        playerRB = GetComponent<Rigidbody2D>(); // obtiene el componente Rigidbody2D del objeto al que está adjuntado este script
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-        moveInput = new Vector2(moveX, moveY);
-
+        float moveX = Input.GetAxisRaw("Horizontal");// obtiene la entrada horizontal (A/D o flechas izquierda/derecha)
+        float moveY = Input.GetAxisRaw("Vertical");// obtiene la entrada vertical (W/S o flechas arriba/abajo)
+        moveInput = new Vector2(moveX, moveY);// crea un vector2 con la entrada de movimiento
+        playerRB.MovePosition(playerRB.position + moveInput * speed * Time.deltaTime);// mueve el Rigidbody2D a la nueva posición calculada
     }
-    private void FixedUpdate()
+    
+    private void OnTriggerStay2D(Collider2D collision)// se llama cuando el collider del objeto con el que colisiona permanece en contacto
     {
-        playerRB.MovePosition(playerRB.position + moveInput * speed * Time.deltaTime);
+        if ((Input.GetKeyDown(KeyCode.H)) && (collision.gameObject.layer == LayerMask.NameToLayer("start"))) // si se presiona la tecla espacio y el objeto con el que colisiona tiene la etiqueta "start"
+            {
+            SceneManager.LoadScene("Room1");// carga la escena llamada "Room1"
+        }   
 
+
+        if ((collision.gameObject.layer == LayerMask.NameToLayer("quit")) && (Input.GetKeyDown(KeyCode.H))) // si el objeto con el que colisiona tiene la etiqueta "quit"
+        {
+            Debug.Log("salir"); //***************   borrar esta linea   *********** 
+            Application.Quit(); // cierra la aplicacion
+        }
+
+       
     }
-
 }
